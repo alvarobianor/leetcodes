29e2d25304c5a0aec8a8b03c50c5bf8d9f40917f
Álvaro Bianor <bianor.alvaro12@gmail.com>
2026-03-26 11:13:46 -0300

    feat: Add LeetCode solution for the Longest Common Prefix problem.

---
+++ longest-common-prefix-14.ts
@@ -0,0 +1,68 @@
+/*Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+ 
+
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters if it is non-empty.
+
+https://leetcode.com/problems/longest-common-prefix/description/?envType=problem-list-v2&envId=array
+
+*/
+
+function longestCommonPrefix(strs: string[]): string {
+  if (strs.length < 1 || strs.length > 200) return "";
+
+  let wordWithMinLength = strs[0];
+
+  let preffix = strs[0];
+
+  strs.forEach((word) => {
+    if (word.length <= 0 || word.length > 200) {
+      return "";
+    }
+
+    if (word.length < wordWithMinLength.length) {
+      wordWithMinLength = word;
+    }
+  });
+
+  strs.forEach((word) => {
+    if (word != wordWithMinLength) {
+      let letters = "";
+      for (let i = 0; i < wordWithMinLength.length; i++) {
+        if (wordWithMinLength[i] === word[i]) {
+          letters += wordWithMinLength[i];
+        } else {
+          break;
+        }
+      }
+      if (letters.length < preffix.length) {
+        preffix = letters;
+      }
+    }
+  });
+
+  return preffix;
+}
+
+console.log(
+  "Out 1: ",
+  longestCommonPrefix(["flower", "finale", "flow", "flight", "flame", "found"]),
+);
+console.log("Out 2: ", longestCommonPrefix(["dog", "racecar", "car"]));
+console.log("Out 3: ", longestCommonPrefix(["car", "cir"]));
