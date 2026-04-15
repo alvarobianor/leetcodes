780d4b93a8a437ae8d0819c53f7fd73bf28fb7a0
Álvaro Bianor <bianor.alvaro12@gmail.com>
2026-04-15 08:36:05 -0300

    feat: add commented-out reference implementation for search in rotated sorted array

--- search-in-rotated-sorted-array-33.ts
+++ search-in-rotated-sorted-array-33.ts
@@ -73,3 +73,32 @@ for (const num of nums) {
 console.log(success);
 
 // my submission: https://leetcode.com/problems/search-in-rotated-sorted-array/submissions/1978737403/
+
+// My code + best solution that I found
+
+// function search(nums: number[], target: number): number {
+//   if (nums.length < 1 || nums.length > 5000) {
+//     return -1;
+//   }
+
+//   let l = 0;
+//   let r = nums.length - 1;
+
+//   while (l <= r) {
+//     let mid = (l + r) >> 1;
+
+//     if (target == nums[mid]) {
+//       return mid;
+//     }
+
+//     if (nums[l] <= nums[mid]) {
+//       if (target >= nums[l] && target < nums[mid]) r = mid - 1;
+//       else l = mid + 1;
+//     } else {
+//       if (target > nums[mid] && target <= nums[r]) l = mid + 1;
+//       else r = mid - 1;
+//     }
+//   }
+
+//   return -1;
+// }
