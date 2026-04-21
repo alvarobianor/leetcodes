4795adab79eb61dfc70af741d74afaef8c207327
Álvaro Bianor <bianor.alvaro12@gmail.com>
2026-04-21 18:35:26 -0300

    feat: implement bracket validation logic using stack-based character tracking

--- valid-parentheses-20.ts
+++ valid-parentheses-20.ts
@@ -42,5 +42,48 @@
 // s consists of parentheses only '()[]{}'.
 
 function isValid(s: string): boolean {
-  return false;
+  if (s.length <= 1 || s.length >= 100000) {
+    return false;
+  }
+
+  const ob1 = "(";
+  const ob2 = "[";
+  const ob3 = "{";
+  const cb1 = ")";
+  const cb2 = "]";
+  const cb3 = "}";
+
+  const openBrackets: string[] = [];
+  const closeBrackets: string[] = [];
+
+  for (const ele of s) {
+    if (ele === ob1 || ele === ob2 || ele === ob3) {
+      openBrackets.push(ele);
+      continue;
+    }
+    if (ele === cb1 || ele === cb2 || ele === cb3) {
+      closeBrackets.push(ele);
+    }
+  }
+
+  if (openBrackets.length !== closeBrackets.length) {
+    return false;
+  }
+
+  console.log(`OB -> ${openBrackets} || CB -> ${closeBrackets}`);
+
+  for (let i = openBrackets.length - 1; i > 0; i--) {
+    const result = `${openBrackets[i]}${closeBrackets[i]}`;
+
+    console.log(`result -> ${result} || i -> ${i}`);
+
+    if (result !== "()" && result !== "[]" && result !== "{}") {
+      return false;
+    }
+  }
+
+  return true;
 }
+const s = "()[[]]";
+
+console.log(`string -> ${s} || result -> ${isValid(s)}`);
